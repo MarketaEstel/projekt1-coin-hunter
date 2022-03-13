ba4ab6c0979b7788080048281789294ecce77064
MarketEstel <estelka.m@gmail.com>
2022-03-13 18:16:46 +0100

    umístěna mince

--- main.js
+++ main.js
@@ -20,12 +20,14 @@ let panacekY = 400;
 let panacekSirka = "64px";
 let panacekVyska = "70px";
 let mince = document.querySelector("#mince");
-let minceX = Math.floor(Math.random()*20);
+let minceX = 600;
 let minceY = 600;
 let minceSirka = "36px";
 let minceVyska = "36px";
-
 let score = 0;
+
+let innerWidth = Window.innerWidth;
+let innerHeight = Window.innerHeight;
 
 // tato funkce se spustí při načtení stránky
 // tj. ve chvíli, kdy je načtené komplet HTML, CSS a všechny obrázky a zvuky
@@ -52,15 +54,18 @@ function krok(hop) {
 			panacek.style.top = panacekY + "px";
 			console.log(panacek.style.top);
 			panacek.src = "obrazky/panacek.png";
+
+		otestujKolizi;
+
 		} //v této funkci musím kontrolovat kolizi panáčka s mincí, v každém kroku if a else if a při každém kroku panáčka, musím volat funkci kolize = otestujKolizi()
 	}
 
 function priNacteniStranky() {
 	panacek.style.left = "400px";
 	panacek.style.top = "400px";
-
-	krok();
 	
+	krok;
+	novaMince();
 
 	// do globálních proměnných si uložíme odkaz na objekty panáčka a mince,
 	// abychom je nemuseli při každém použití znovu na stránce hledat pomocí document.querySelector
@@ -70,8 +75,8 @@ function priNacteniStranky() {
 	// umístíme panáčka na startovní pozici
 	// zjistíme šířku a výšku mince
 	// a vygenerujeme první minci na náhodné pozici
-
 }
+
 
 // funkce, která umístí panáčka na jeho souřadnice
 // tj. nastaví jeho style.left a style.top na hodnoty panacekX, panacekY
@@ -81,13 +86,20 @@ function priNacteniStranky() {
 
 // funkce pro nahodné vygenerování nové pozice mince
 // a umístění mince na tyto souřadnice
-	function novaMince() {
-		score += 1; 
-		document.querySelector("#score").textContent = score;
 
-		//musím použít funkci Math.floor(Math.random()*20+1), hodnoty randomu musí být max. velikosti plochy a dělitelné 20 (krok panáčka)
-		//zde musím použít score = jakmile se sebere mince, skore se zvětší o jedničku. Udělám si proměnnou pro skore. Do obsahu skore musím psát reálné skore
+function novaMince() {
+	document.querySelector("#score").textContent = score;
+	score += 1;
 	
+	minceX = (Math.random()*innerWidth+1);
+	mince.style.left = minceX + "px";
+	minceY = (Math.random()*innerHeight+1);
+	mince.style.top = minceY + "px";
+				
+	console.log("novaMince");
+
+		//musím použít funkci Math.floor(Math.random()*šířka/výška okna prohlížeče+1), hodnoty randomu musí být max. velikosti plochy
+		//zde musím použít score = jakmile se sebere mince, skore se zvětší o jedničku. Udělám si proměnnou pro skore. Do obsahu skore musím psát reálné skore
 	}
 
 // tato funkce se zavolá při stisku klávesy
@@ -117,10 +129,14 @@ function priNacteniStranky() {
 //}
 
 // fuknce pro otestování kolize panáčka s mincí
-// function otestujKolizi() {
 
-// 	if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-// 		// panacek a mince se prekryvaji
-// 	}
-//}
+function otestujKolizi() {
+	console.log(otestujKolizi)
+	if (( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) 
+	novaMince();
+	krok;
+	}
+
+		// 		// panacek a mince se prekryvaji
+
 // 	když se rovnají pozice panáčka a mince dojde ke kolizi, mince se přemístí na novou pozici = musím si zavolat funkci mince, kde mám random, zvuk a score
